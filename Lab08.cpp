aaffba604bbad448234a10fcfcaad30a4162c736
austinlang748 <74215480+austinlang748@users.noreply.github.com>
2021-03-13 16:17:06 -0700

    changed lab8.cpp

--- Lab08.cpp
+++ Lab08.cpp
@@ -148,7 +148,7 @@ int main(int argc, char** argv)
 #endif // !_WIN32
 {
    // run test
-   bool runTest = false;
+   bool runTest = true;
    if (runTest) {
       testRunner();
       return 0; // quick exit
